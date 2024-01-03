585df3a3670aaa111746f0abba8e12692efe2311
Fatima Arrillaga Garcia <fathymarial2@gmail.com>
2024-01-03 14:29:05 -0600

    feature: scroll de mentorias

--- app/routes/mentorias1a1/Hero.jsx
+++ app/routes/mentorias1a1/Hero.jsx
@@ -4,15 +4,17 @@ import BgIllustration from "~/components/BgIlustration/BgIlustration";
 
 export default function MentoriasHero() {
   return (
-    <section >
-      <BgIllustration />
+    <section>
+      <div className="relative max-w-6xl mx-auto pb-0 px-4 sm:px-6">
+        <BgIllustration />
 
-      <HeroSection
-        title="Mentorías One to One personalizadas"
-        description="⏳🌟 Ama y cuida tu recurso máximo, no renovable: tu tiempo. Crea proyectos reales; la práctica hace al maestro, evita agotarte con información innecesaria. 👩‍💻👨‍💻 Desarrolladores de calidad para crear productos de calidad."
-        buttonText="Regístrate"
-        buttonLink="./registro"
-      />
+        <HeroSection
+          title="Mentorías One to One personalizadas"
+          description="⏳🌟 Ama y cuida tu recurso máximo, no renovable: tu tiempo. Crea proyectos reales; la práctica hace al maestro, evita agotarte con información innecesaria. 👩‍💻👨‍💻 Desarrolladores de calidad para crear productos de calidad."
+          buttonText="Regístrate"
+          buttonLink="./registro"
+        />
+      </div>
     </section>
   );
 }
